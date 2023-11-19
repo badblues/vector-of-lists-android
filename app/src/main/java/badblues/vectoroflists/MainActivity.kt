1a7dc7be8018ce00239acb51976f42df0e02a93f
badblues <borovikxv.a@gmail.com>
2023-11-19 17:44:51 +0700

    finished this abomination

--- app/src/main/java/badblues/vectoroflists/MainActivity.kt
+++ app/src/main/java/badblues/vectoroflists/MainActivity.kt
@@ -3,30 +3,29 @@ package badblues.vectoroflists
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import badblues.vectoroflists.ui.theme.VectorOfListsAndroidTheme
 import badblues.vectoroflists.model.DataModel
 import badblues.vectoroflists.model.DataTypes
 import badblues.vectoroflists.datastructure.Vector2D
-import badblues.vectoroflists.datastructure.VectorOfLists
-import androidx.compose.ui.unit.dp
 import java.io.*
 import java.util.*
 
 class MainActivity : ComponentActivity() {
     private val model: DataModel = DataModel.getInstance()
     private lateinit var sharedPreferences: SharedPreferences
+
 
     @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +46,11 @@ class MainActivity : ComponentActivity() {
         loadVectorData()
     }
 
+    override fun onStop() {
+        super.onStop()
+        handleSaveVector()
+    }
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun AppContent() {
@@ -55,91 +59,238 @@ class MainActivity : ComponentActivity() {
         var insertInputText by remember { mutableStateOf("") }
         var changeBaseCapacityText by remember { mutableStateOf("") }
         var addRandomInputText by remember { mutableStateOf("") }
+        var vectorText by remember { mutableStateOf(if (model.getCurrentType() == DataTypes.Double) model.getDoublesVector().toString() else model.getVectors2DVector().toString()) }
+        var currentDataType by remember { mutableStateOf(model.getCurrentType().toString()) }
 
-        val context = LocalContext.current
+        fun updateText() {
+            vectorText = if (model.getCurrentType() == DataTypes.Double) model.getDoublesVector().toString() else model.getVectors2DVector().toString()
+        }
 
-        Column(
+
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
         ) {
-            TextField(
-                value = addInputText,
-                onValueChange = { addInputText = it },
-                label = { Text("Add") },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Done,
-                    keyboardType = KeyboardType.Text
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        handleAdd(context, addInputText)
-                        addInputText = ""
-                    }
+            item {
+                Text(
+                    text = vectorText
                 )
-            )
-
-            // Other UI elements go here...
-
-            Button(
-                onClick = { handleAdd(context, addRandomInputText) },
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text("Add Random")
             }
 
-            Button(
-                onClick = { handleDelete(context, deleteInputText) },
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text("Delete")
+            item {
+                Row {
+                    TextField(
+                        value = addInputText,
+                        onValueChange = {
+                            if(isValidElementInput(it))
+                                addInputText = it
+                        },
+                        label = { Text("Add") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Done,
+                            keyboardType = KeyboardType.Text
+                        ))
+                    Button(
+                        onClick = {
+                            handleAdd(addInputText)
+                            addInputText = ""
+                            updateText()
+                        },
+                    ) {
+                        Text("ADD")
+                    }
+                }
             }
 
-            Button(
-                onClick = { handleSort() },
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text("Sort")
+            item {
+                Row {
+                    TextField(
+                        value = insertInputText,
+                        onValueChange = {
+                            if (isValidIntegerInput(it))
+                                insertInputText = it
+                        },
+                        label = { Text("Position") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Done,
+                            keyboardType = KeyboardType.Text
+                        ))
+                    Button(
+                        onClick = {
+                            handleInsert(insertInputText, addInputText)
+                            insertInputText = ""
+                            addInputText = ""
+                            updateText()
+                        },
+                    ) {
+                        Text("INSERT")
+                    }
+                }
+
             }
 
-            Button(
-                onClick = { handleClear() },
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text("Clear")
+
+            item {
+                Row {
+                    TextField(
+                        value = deleteInputText,
+                        onValueChange = {
+                            if (isValidIntegerInput(it))
+                                deleteInputText = it },
+                        label = { Text("Position") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Done,
+                            keyboardType = KeyboardType.Text
+                        ))
+                    Button(
+                        onClick = {
+                            handleDelete(deleteInputText)
+                            deleteInputText=""
+                            updateText()
+                        },
+                    ) {
+                        Text("DELETE")
+                    }
+                }
+
             }
 
-            Button(
-                onClick = { handleChangeBaseCapacity(changeBaseCapacityText) },
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text("Change Base Capacity")
+            item {
+                Row {
+                    TextField(
+                        value = addRandomInputText,
+                        onValueChange = {
+                            if (isValidIntegerInput(it))
+                                addRandomInputText = it },
+                        label = { Text("N") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Done,
+                            keyboardType = KeyboardType.Text
+                        ))
+                    Button(
+                        onClick = {
+                            handleAddRandom(addRandomInputText)
+                            addRandomInputText = ""
+                            updateText()
+                        }
+                    ) {
+                        Text("ADD N")
+                    }
+                }
             }
 
-            Button(
-                onClick = { handleSaveVector() },
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text("Save Vector")
+
+            item {
+                Row {
+                    TextField(
+                        value = changeBaseCapacityText,
+                        onValueChange = {
+                            if (isValidIntegerInput(it))
+                                changeBaseCapacityText = it },
+                        label = { Text("Capacity") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Done,
+                            keyboardType = KeyboardType.Text
+                        ))
+                    Button(
+                        onClick = {
+                            handleChangeBaseCapacity(changeBaseCapacityText)
+                            updateText()
+                        },
+                    ) {
+                        Text("CHANGE CAPACITY")
+                    }
+                }
+
             }
 
-            Button(
-                onClick = { handleOpenVector() },
-                modifier = Modifier.padding(top = 16.dp)
-            ) {
-                Text("Open Vector")
+
+            item {
+                Button(
+                    onClick = {
+                        handleSort()
+                        updateText()
+                    }
+                ) {
+                    Text("SORT")
+                }
             }
 
+            item {
+
+                Button(
+                    onClick = {
+                        handleClear()
+                        updateText()
+                    }
+                ) {
+                    Text("CLEAR")
+                }
+            }
+
+
+            item {
+                Row {
+                    Text(currentDataType)
+                    Button(
+                        onClick = {
+                            handleChangeDataType()
+                            updateText()
+                            currentDataType = model.getCurrentType().toString()
+                        }
+                    ) {
+                        Text("CHANGE TYPE")
+                    }
+                }
+            }
         }
     }
 
-    private fun handleAdd(context: Context, inputText: String) {
-        // Handle add logic here
-        // You can use the 'model' instance to interact with your existing data model
+    private fun handleAdd(addText: String) {
+        if (addText.isBlank())
+            return
+        try {
+            if (model.getCurrentType() == DataTypes.Double)
+                model.getDoublesVector().add(addText.toDouble())
+            else
+                model.getVectors2DVector().add(Vector2D.parseVector2d(addText))
+        } catch (e: Exception) {
+            e.message?.let { showToast(it) }
+        }
     }
 
-    private fun handleDelete(context: Context, inputText: String) {
-        // Handle delete logic here
+    private fun handleInsert(insertText: String, addText: String) {
+        if (addText.isBlank() || insertText.isBlank())
+            return
+        if (model.getCurrentType() == DataTypes.Double)
+            model.getDoublesVector().insert(insertText.toInt(), addText.toDouble())
+        else
+            model.getVectors2DVector().insert(insertText.toInt(), Vector2D.parseVector2d(addText))
+    }
+
+    private fun handleDelete(deleteText: String) {
+        if (deleteText.isBlank())
+            return
+        val position = deleteText.toInt()
+        if (model.getCurrentType() == DataTypes.Double)
+            model.getDoublesVector().delete(position)
+        else
+            model.getVectors2DVector().delete(position)
+    }
+
+    private fun handleAddRandom(addRandomText: String) {
+        if (addRandomText.isBlank())
+            return
+        val n = addRandomText.toInt()
+        val random = Random()
+        if (model.getCurrentType() == DataTypes.Double)
+            for (i in 0 until n)
+                model.getDoublesVector().add(random.nextInt(100).toDouble())
+        else
+            for (i in 0 until n) {
+                val vector2d = Vector2D(random.nextInt(100).toDouble(), random.nextInt(100).toDouble())
+                model.getVectors2DVector().add(vector2d)
+            }
     }
 
     private fun handleSort() {
@@ -147,19 +298,21 @@ class MainActivity : ComponentActivity() {
             model.getDoublesVector().sort()
         else
             model.getVectors2DVector().sort()
-        updateText()
     }
 
     private fun handleClear() {
         model.clearVectors()
-        updateText()
     }
 
     private fun handleChangeBaseCapacity(newCapacity: String) {
-        if (newCapacity.isNotEmpty()) {
-            model.changeBaseCapacities(newCapacity.toInt())
-            updateText()
-        }
+        if (newCapacity.isBlank())
+            return
+        model.changeBaseCapacities(newCapacity.toInt())
+    }
+
+    private fun handleChangeDataType() {
+        var newType = if (model.getCurrentType() == DataTypes.Double) DataTypes.Vector2D else DataTypes.Double
+        model.setCurrentType(newType)
     }
 
     private fun handleSaveVector() {
@@ -169,7 +322,10 @@ class MainActivity : ComponentActivity() {
             else
                 model.getVectors2DVector().toString()
 
+            val type = model.getCurrentType().toString()
+
             sharedPreferences.edit().putString(KEY_VECTOR_DATA, jsonString).apply()
+            sharedPreferences.edit().putString(KEY_VECTOR_TYPE, type).apply()
             showToast("Vector saved successfully")
         } catch (e: Exception) {
             showToast("Error saving vector: ${e.message}")
@@ -178,44 +334,43 @@ class MainActivity : ComponentActivity() {
 
     private fun handleOpenVector() {
         try {
+            val typeString = sharedPreferences.getString(KEY_VECTOR_TYPE, "")
             val jsonString = sharedPreferences.getString(KEY_VECTOR_DATA, "")
-            if (jsonString.isNullOrEmpty()) {
+            if (jsonString.isNullOrEmpty() || typeString.isNullOrEmpty()) {
                 showToast("No saved vector found")
                 return
             }
-
-            if (model.getCurrentType() == DataTypes.Double) {
-                // Parse and set Double vector
-                // Example: "[1.0, 2.0, 3.0]"
+            val type = DataTypes.valueOf(typeString)
+            model.setCurrentType(type)
+            if (type == DataTypes.Double) {
                 val elements = jsonString
+                    .replace("\n", ", ")
                     .trim('[', ']')
-                    .split(",")
+                    .split(", ")
                     .map { it.trim().toDouble() }
-                val doubleVector = VectorOfLists<Double>(2)
-                model.setDoublesVector(doubleVector)
+                for (elem in elements)
+                    model.getDoublesVector().add(elem)
             } else {
-                // Parse and set Vector2D vector
-                // Example: "[(1.0, 2.0), (3.0, 4.0)]"
                 val elements = jsonString
+                    .replace("\n", ", ")
                     .trim('[', ']')
                     .split("),")
                     .map {
-                        val coords = it.trim('(', ')').split(",")
+                        val coords = it.replace(" ", "").trim('(', ')').split(",")
                         Vector2D(coords[0].toDouble(), coords[1].toDouble())
                     }
-                val vector2DVector = VectorOfLists<Vector2D>(2)
-                model.setVectors2DVector(vector2DVector)
+                for (elem in elements)
+                    model.getVectors2DVector().add(elem)
             }
 
             showToast("Vector loaded successfully")
-            updateText()
         } catch (e: Exception) {
             showToast("Error loading vector: ${e.message}")
         }
     }
 
     private fun showToast(message: String) {
-        // Implement your showToast logic here
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
     private fun loadVectorData() {
@@ -223,13 +378,22 @@ class MainActivity : ComponentActivity() {
         handleOpenVector()
     }
 
-    private fun updateText() {
-        // Implement your updateText logic here
+    private fun isValidElementInput(inputText: String): Boolean {
+        val regex = if (model.getCurrentType() == DataTypes.Double) {
+            Regex("-?\\d*\\.?\\d*")
+        } else {
+            Regex("-?\\d*(\\.)?(\\d+)?(,)?(-?\\d+(\\.)?(\\d+)?)?")
+        }
+        return regex.matches(inputText)
     }
 
-    // ... (Other methods remain the same)
+    private fun isValidIntegerInput(inputText: String): Boolean {
+        val regex = Regex("\\d*")
+        return regex.matches(inputText)
+    }
 
     companion object {
         const val KEY_VECTOR_DATA = "vector_data"
+        const val KEY_VECTOR_TYPE = "vector_type"
     }
 }
